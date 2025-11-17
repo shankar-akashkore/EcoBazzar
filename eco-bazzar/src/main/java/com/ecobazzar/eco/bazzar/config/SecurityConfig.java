54c77853b09b8a90943961ccd7a2ecd1497b0ee8
shankar akashkore <shankarakashkore32@gmail.com>
2025-11-17 19:39:15 +0530

    Updated SecurityConfig

--- eco-bazzar/src/main/java/com/ecobazzar/eco/bazzar/config/SecurityConfig.java
+++ eco-bazzar/src/main/java/com/ecobazzar/eco/bazzar/config/SecurityConfig.java
@@ -14,11 +14,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.ecobazzar.eco.bazzar.security.JwtFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
-	
-	private final JwtFilter jwtFilter;
+
+    private final JwtFilter jwtFilter;
 
     public SecurityConfig(JwtFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
@@ -26,29 +29,41 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterConfig(HttpSecurity http) throws Exception {
-    	http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-            		.requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
-            		.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-            		.requestMatchers(HttpMethod.GET, "/api/products/**", "/products/**").permitAll()
-            		.requestMatchers(HttpMethod.POST, "/api/products/**", "/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
-            		.requestMatchers(HttpMethod.PUT, "/api/products/**", "/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
-            		.requestMatchers(HttpMethod.DELETE, "/api/products/**", "/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
-            		.requestMatchers("/api/cart/**", "/api/checkout/**", "/api/orders/**").hasAuthority("ROLE_USER")
-            		.requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
-            		.anyRequest().authenticated()
-            		)
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-            .formLogin(form -> form.disable())
-            .httpBasic(basic -> basic.disable());
-    	
-    	return http.build();
+        http
+                .cors(cors -> {})
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/products/**").hasAnyAuthority("ROLE_SELLER", "ROLE_ADMIN")
+                        .requestMatchers("/api/cart/**", "/api/checkout/**", "/api/orders/**").hasAuthority("ROLE_USER")
+                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable());
+        return http.build();
     }
-    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
--- eco-bazzar/src/main/java/com/ecobazzar/eco/bazzar/controller/AuthController.java
+++ eco-bazzar/src/main/java/com/ecobazzar/eco/bazzar/controller/AuthController.java
@@ -1,10 +1,7 @@
 package com.ecobazzar.eco.bazzar.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ecobazzar.eco.bazzar.dto.UserRespone;
 import com.ecobazzar.eco.bazzar.dto.LoginRequest;
 import com.ecobazzar.eco.bazzar.dto.RegisterRequest;
@@ -12,6 +9,7 @@ import com.ecobazzar.eco.bazzar.service.AuthService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
